e2a07f663b9bb183b52402f6452843656c44df7c
Gunnar Quehl <github@gunnar-quehl.de>
2012-01-05 22:23:38 +0100

    added the base Crawler class
    a crawler contains a
    - Stack (parameters for function calls) - StackItem created but not yet defined
    - Path  (will crawling along a properties (and function calls) the path records the nodes that were pasted).
    
    Stack and Pathes are defined in a functional way. They are SingleLinkedLists.
    a single linked list has no public contructor. One always starts with an empty list:
    
    var empty = SingleLinkedList<object>.Empty;
    
    then one can get a longer list by adding items. Adding return another longer
    linked list. The original is not touched.
    
    var one = empty.Add("something");
    
    List can be taken appart by the properties (Head and Tail);
    
    var emptyAgain = one.Tail;
    var something  = one.Head;

---
+++ Ananse/Crawler/Crawler.cs
@@ -0,0 +1,47 @@
+// 
+//  Crawler.cs
+//  
+//  Author:
+//       Gunnar Quehl <github@gunnar-quehl.de>
+// 
+//  Copyright (c) 2012 Gunnar Quehl
+// 
+//  This library is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as
+//  published by the Free Software Foundation; either version 2.1 of the
+//  License, or (at your option) any later version.
+// 
+//  This library is distributed in the hope that it will be useful, but
+//  WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//  Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+using System;
+using System.Collections.Generic;
+
+namespace Ananse
+{
+	public abstract class Crawler
+	{
+		public Crawler (object tag)
+		{
+			Stack = SingleLinkedList<StackItem>.Empty;
+			Path  = SingleLinkedList<Crawler>.Empty;
+			Tag   = tag;
+		}
+		
+		public abstract Type[] Signature   			{ get; }
+		
+		public object Tag  							{ get; private set; }
+		
+		public SingleLinkedList<StackItem> Stack 	{ get; private set; }
+		public SingleLinkedList<Crawler>   Path  	{ get; private set; }
+		
+		public abstract IEnumerator<string> Keys 	{ get; }
+		public abstract Crawler this[string key] 	{ get; }
+	}
+}
+
